83fba178cc244c7290d1b0e7856d291afcec3584
sirisha <sirisha.bubbly57@gmail.com>
2017-04-19 19:23:54 -0400

    java first program

---
+++ new 2.java
@@ -0,0 +1,10 @@
+class Calculator {
+    public static void main(String[] args) {
+        int x = 10;
+        float y = 20;
+        System.out.println("Addition = " + (x+y));
+        System.out.println("Substraction = " + (x-y));
+        System.out.println("Multiplication = " + (x*y));
+        System.out.println("Division = " + (x/y));
+    }
+}
