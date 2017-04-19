a7d27b961aaa2ed3fa0e10d651bdce39ca750bad
CHIKKI\greeshmadeepika <greeshmadeepika7@gmail.com>
2017-04-19 18:57:59 -0400

    java files

---
+++ Comp.java
@@ -0,0 +1,9 @@
+class Comp{
+public static void main(String[] args){
+char ch1='a';
+ int a=1;
+char ch2='b';
+ int b=2;
+System.out.println("comparision" + (ch1==ch2));
+}
+}
---
+++ Practice.java
@@ -0,0 +1,7 @@
+class Practice{
+public static void main(String[] args){
+int a=10;
+float b=20;
+System.out.println("Addition=" + (a+b));
+}
+}
