7c81d05ff9f812b3442bc78650c3f1da87cc2259
LAPTOP-644I68BU\rnuta <nrajitha37@gmail.com>
2017-04-20 10:11:32 -0400

    commit swap

---
+++ Swap.java
@@ -0,0 +1,12 @@
+class Swap {
+      public static void main (String args[]) {
+	           int a=7;
+			   int b=9;
+			   a=a+b;
+			   b=a-b;
+			   a=a-b;
+			   System.out.println("a="+a);
+			   System.out.println("b="+b);
+			  
+			   }
+			}   
