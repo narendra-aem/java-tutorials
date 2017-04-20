1739650c356cbd96d6388e23b26c49223debc225
MEHJABEEN\smehj <smehjabeen9@gmail.com>
2017-04-20 11:00:09 -0400

    modulo

---
+++ Modulo.java
@@ -0,0 +1,17 @@
+class Modulo {
+
+   public static void main(String args[])
+   {
+      float a=3;
+	  int b=2;
+	  
+      System.out.println("Value of a: " + a);
+	  System.out.println("Value of b: " + b);
+	  
+	  a=a%b;
+	  System.out.println("Value of a: " + a);
+	  
+	  
+	  
+   }
+}
