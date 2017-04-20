f73962eee6dca9cf1344bea8e0b3bf7376566ebb
LAPTOP-644I68BU\rnuta <nrajitha37@gmail.com>
2017-04-20 11:39:49 -0400

    commit modulo

---
+++ Modulo.java
@@ -0,0 +1,8 @@
+class Modulo{
+         public static void main(String args[]) {
+		                   int p=6;
+						   int k=3;
+				           p=p%k;
+				System.out.println("p="+p);
+				}
+			}	
---
+++ Swapn.java
@@ -0,0 +1,12 @@
+class Swapn {
+      public static void main(String args[]) {
+	                int r=-5;
+					int s=-3;
+					r=r+s;
+					s=r-s;
+					r=r-s;
+				System.out.println("r="+r);
+				System.out.println("s="+s);
+				}
+		}		
+					
